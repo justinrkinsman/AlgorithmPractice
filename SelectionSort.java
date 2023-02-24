086704f540d444bb3a003125ea1651a1fa87689c
Justin <justinrkinsman@gmail.com>
2023-02-24 08:47:22 -0400

    Create file for SelectionSort and InsertionSort

---
+++ SelectionSort.java
@@ -0,0 +1,7 @@
+public class SelectionSort { // O(n^2) even in the best case (n-1) + (n-2) + (n-3) + (n-4)....
+    /*
+     * Place pointer at first element. Find the smallest index of the remaining
+     * indices.
+     * Swap it with current pointer. Repeat.
+     */
+}
